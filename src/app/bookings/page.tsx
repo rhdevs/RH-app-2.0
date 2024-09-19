7a3e6ee850a4dae40e8ce2ba3250b0060134dc87
keithxun <keithonghx@gmail.com>
2024-09-19 17:16:04 +0800

    Abstract header component

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../_components/header";
 
 const BookingsPage: React.FC = () => {
   return (
     <div>
+      <Header />
       <h1>Bookings Page</h1>
       {/* Add your booking components and logic here */}
     </div>
