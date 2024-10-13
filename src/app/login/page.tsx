281c54c5d6134c45097fb04739ff40c43e203e64
HiokKuek <ernest.tanhk@gmail.com>
2024-10-13 14:35:41 +0800

    added authentication using auth0

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,42 @@
 "use client";
-
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import { signIn } from "next-auth/react";
 
 const LoginPage: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  // const router = useRouter();
+  // useEffect(() => {
+  //   if (session) {
+  //     router.push("/profile"); // Change to your target route
+  //   }
+  // }, [router]);
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-  };
+  // const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setUsername(event.target.value);
+  // };
 
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
+  // const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setPassword(event.target.value);
+  // };
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Add your login logic here
-  };
+  // const handleSubmit = (event: React.FormEvent) => {
+  //   event.preventDefault();
+  //   // Add your login logic here
+  // };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
+    <div className="flex flex-col items-center justify-center gap-8 p-12">
+      <h2 className="font-bold">Hey There!</h2>
+
+      <button
+        className="rounded-md border border-zinc-300 bg-white px-6 py-2 text-zinc-700"
+        onClick={() => signIn("google")}
+      >
+        <span className="mr-2 font-bold text-red-700">G</span> Sign in with
+        Google
+      </button>
+      {/* <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
@@ -42,7 +56,7 @@ const LoginPage: React.FC = () => {
           />
         </div>
         <button type="submit">Login</button>
-      </form>
+      </form> */}
     </div>
   );
 };
