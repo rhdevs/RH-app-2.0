536edd89d54f497d0a9f18f18d1c75b99c46be27
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2024-12-09 22:29:33 +0700

    Fix login page UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,14 +20,15 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="flex flex-col align-center justify-center text-center p-10">
+      <h2 className="font-bold text-black text-lg">Login</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
             type="text"
             id="username"
+            className="border-2 rounded"
             value={username}
             onChange={handleUsernameChange}
           />
@@ -37,11 +38,14 @@ const LoginPage: React.FC = () => {
           <input
             type="password"
             id="password"
+            className="border-2 rounded"
             value={password}
             onChange={handlePasswordChange}
           />
         </div>
-        <button type="submit">Login</button>
+        <button className="inline-block justify-center align-center text-center text-black bg-customBlue border border-black rounded-3xl px-5 min-h-10 hover:bg-customWhite hover:text-customBlue hover:scale-110">
+          Login
+        </button>
       </form>
     </div>
   );
