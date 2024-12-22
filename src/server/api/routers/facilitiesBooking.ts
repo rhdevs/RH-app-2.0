e2f53db1c764e68e4190a012bd1ff104065656ab
Lu Xinming <xinmingsm11@gmail.com>
2024-12-22 15:02:16 +0800

    fix type error from update schema

--- src/server/api/routers/facilitiesBooking.ts
+++ src/server/api/routers/facilitiesBooking.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const facilityBookingRouter = createTRPCRouter({
   // Get all facilities in ascending order
