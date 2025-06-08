92a886667af25ab80ee5a0caa431b25a097cad4a
puma-31 <mpurav2006@gmail.com>
2025-06-08 13:20:19 +0530

    updated login and signup page design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Link } from "react-router-dom";
 import { signIn } from "next-auth/react";
 import { useForm, type SubmitHandler } from "react-hook-form";
 
@@ -35,63 +36,138 @@ const LoginPage = () => {
     signIn("credentials", { ...data });
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8 p-12">
-      <h2 className="font-bold">
-        Welcome to Raffles Hall! Please sign in to continue
-      </h2>
+    <div className="relative flex min-h-screen items-center justify-center">
+      <div
+        className="absolute inset-0 z-0 bg-cover"
+        style={{
+          backgroundImage: 'url("rh-sunset.png")',
+          // filter: "blur(4px)",
+        }}
+      ></div>
 
-      <form className="flex flex-col items-center gap-6">
-        <input
-          type="text"
-          placeholder="Email"
-          className="rounded-md border border-zinc-300 px-6 py-2"
-          {...register("email")}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="rounded-md border border-zinc-300 px-6 py-2"
-          {...register("password")}
-        />
-        <button
-          type="submit"
-          className="rounded-md border border-zinc-300 bg-white px-6 py-2 text-zinc-700"
-          onClick={handleSubmit(onSubmit)}
-        >
-          Login
-        </button>
-      </form>
+      <div className="relaive z-10 w-full max-w-md overflow-hidden rounded-xl bg-white shadow-lg">
+        {/* Top Image Section */}
+        <div
+          className="h-56 w-full bg-cover bg-center"
+          style={{
+            backgroundImage: 'url("raffles-hall.png")',
+          }}
+        ></div>
 
-      <button
-        className="rounded-md border border-zinc-300 bg-white px-6 py-2 text-zinc-700"
-        disabled={true}
-        onClick={() => signIn("google")}
-      >
-        <span className="mr-2 font-bold text-red-700">G</span> Sign in with
-        Google
-      </button>
-      {/* <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={handleUsernameChange}
-          />
+        {/* Form Section */}
+        <div className="p-6">
+          <h2 className="mb-6 text-3xl font-bold text-gray-800">Login</h2>
+
+          <form className="space-y-4">
+            <div>
+              <input
+                type="text"
+                placeholder="Email"
+                className="bg-white-200 placeholder-grey-700 w-full rounded-[1vw] border px-4 py-2 text-black focus:outline-none"
+                {...register("email")}
+              />
+            </div>
+
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                className="bg-white-200 placeholder-grey-700 w-full rounded-[1vw] border px-4 py-2 text-black focus:outline-none"
+                {...register("password")}
+              />
+            </div>
+
+            {/* <div className="flex items-center justify-between text-sm text-gray-600">
+              <label className="flex items-center">
+                <input type="checkbox" className="mr-2" /> remember me
+              </label>
+              <a href="#" className="hover:underline">
+                forgot password
+              </a>
+            </div> */}
+
+            <button
+              type="submit"
+              className="w-full rounded bg-green-700 py-2 text-white hover:bg-green-800"
+              onClick={handleSubmit(onSubmit)}
+            >
+              Login
+            </button>
+            <button
+              className="w-full rounded border bg-white py-2 text-zinc-700 hover:bg-zinc-200"
+              disabled={true}
+              onClick={() => signIn("google")}
+            >
+              <span className="mr-2 font-bold text-red-700">G</span> Sign in
+              with Google
+            </button>
+          </form>
+
+          <div className="mt-4 text-center">
+            <a href="/signup" className="text-sm text-gray-600 hover:underline">
+              Create Account
+            </a>
+          </div>
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form> */}
+      </div>
     </div>
+    // <div className="flex flex-col items-center justify-center gap-8 p-12">
+    //   <h2 className="font-bold">
+    //     Welcome to Raffles Hall! Please sign in to continue
+    //   </h2>
+
+    //   <form className="flex flex-col items-center gap-6">
+    //     <input
+    //       type="text"
+    //       placeholder="Email"
+    //       className="rounded-md border border-zinc-300 px-6 py-2"
+    //       {...register("email")}
+    //     />
+    //     <input
+    //       type="password"
+    //       placeholder="Password"
+    //       className="rounded-md border border-zinc-300 px-6 py-2"
+    //       {...register("password")}
+    //     />
+    //     <button
+    //       type="submit"
+    //       className="rounded-md border border-zinc-300 bg-white px-6 py-2 text-zinc-700"
+    //       onClick={handleSubmit(onSubmit)}
+    //     >
+    //       Login
+    //     </button>
+    //   </form>
+
+    //   <button
+    //     className="rounded-md border border-zinc-300 bg-white px-6 py-2 text-zinc-700"
+    //     disabled={true}
+    //     onClick={() => signIn("google")}
+    //   >
+    //     <span className="mr-2 font-bold text-red-700">G</span> Sign in with
+    //     Google
+    //   </button>
+    //   {/* <form onSubmit={handleSubmit}>
+    //     <div>
+    //       <label htmlFor="username">Username:</label>
+    //       <input
+    //         type="text"
+    //         id="username"
+    //         value={username}
+    //         onChange={handleUsernameChange}
+    //       />
+    //     </div>
+    //     <div>
+    //       <label htmlFor="password">Password:</label>
+    //       <input
+    //         type="password"
+    //         id="password"
+    //         value={password}
+    //         onChange={handlePasswordChange}
+    //       />
+    //     </div>
+    //     <button type="submit">Login</button>
+    //   </form> */}
+    // </div>
   );
 };
 
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -20,24 +20,107 @@ const SignupPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Signup Page</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={handleEmailChange} />
+    <div className="relative flex min-h-screen items-center justify-center">
+      <div
+        className="absolute inset-0 z-0 bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: 'url("rh-sunset.png")',
+          filter: "blur(4px)",
+        }}
+      ></div>
+
+      <div className="relaive z-10 w-full max-w-md overflow-hidden rounded-xl bg-white shadow-lg">
+        {/* Top Image Section */}
+        <div
+          className="h-56 w-full bg-cover bg-center"
+          style={{
+            backgroundImage: 'url("raffles-hall.png")',
+          }}
+        ></div>
+
+        {/* Form Section */}
+        <div className="p-6">
+          <h2 className="mb-6 text-3xl font-bold text-gray-800">
+            Create Account
+          </h2>
+
+          <form className="space-y-4">
+            <div>
+              <input
+                type="text"
+                placeholder="Email"
+                className="bg-white-200 placeholder-grey-700 w-full rounded-[1vw] border px-4 py-2 text-black focus:outline-none"
+                // {...register("email")}
+              />
+            </div>
+
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                className="bg-white-200 placeholder-grey-700 w-full rounded-[1vw] border px-4 py-2 text-black focus:outline-none"
+                // {...register("password")}
+              />
+            </div>
+
+            {/* <div className="flex items-center justify-between text-sm text-gray-600">
+                  <label className="flex items-center">
+                    <input type="checkbox" className="mr-2" /> remember me
+                  </label>
+                  <a href="#" className="hover:underline">
+                    forgot password
+                  </a>
+                </div> */}
+
+            <button
+              type="submit"
+              className="w-full rounded bg-green-700 py-2 text-white hover:bg-green-800"
+              // onClick={handleSubmit(onSubmit)}
+            >
+              Sign Up
+            </button>
+            <button
+              className="w-full rounded border bg-white py-2 text-zinc-700 hover:bg-zinc-200"
+              disabled={true}
+              // onClick={() => signIn("google")}
+            >
+              <span className="mr-2 font-bold text-red-700">G</span> Sign Up
+              with Google
+            </button>
+          </form>
+
+          <div className="mt-4 text-center">
+            <span className="text-sm text-gray-600">
+              Already have an account?{" "}
+            </span>
+            <a
+              href="/login"
+              className="text-sm text-emerald-800 hover:underline"
+            >
+              Login
+            </a>
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
+      </div>
     </div>
+    // <div>
+    //   <h2>Signup Page</h2>
+    //   <form onSubmit={handleSubmit}>
+    //     <div>
+    //       <label>Email:</label>
+    //       <input type="email" value={email} onChange={handleEmailChange} />
+    //     </div>
+    //     <div>
+    //       <label>Password:</label>
+    //       <input
+    //         type="password"
+    //         value={password}
+    //         onChange={handlePasswordChange}
+    //       />
+    //     </div>
+    //     <button type="submit">Sign Up</button>
+    //   </form>
+    // </div>
   );
 };
 
